e80c0c664c50246e02067af605c1f8c0caab3ebb
sanghyeok-han <53252712+sanghyeok-han@users.noreply.github.com>
2020-08-15 19:04:40 +0900

    immer

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import produce from 'immer'
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
-window.produce = produce
 
 function countActiveUsers(users) {
-  // console.log("활성 사용자 수 셈")
   return users.filter(uesr => uesr.active).length
 }
 
@@ -37,21 +35,17 @@ function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
       return {
-        inputs: initialState.inputs,
         users: state.users.concat(action.user)
       }
+
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id
-            ? { ...user, active: !user.active }
-            : user
-        )
-      }
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id)
+        user.active = !user.active
+      })
+
     case 'REMOVE_USER':
       return {
-        ...state,
         users: state.users.filter(user =>
           user.id !== action.id)
       }
